66a745f4d9d03c67e9564f2ae36b3a9267c3b60b
vitorMalvestiti <68360574+vitorMalvestiti@users.noreply.github.com>
2023-02-06 16:19:37 -0300

    testes scss

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -58,6 +58,73 @@ export class DashboardComponent implements OnInit {
 
 
 
+
+
+
+
+
+
+
+    var chart = new Chart('meuCanvas5', {
+      type: 'pie',
+      data: {
+        labels: ['2019', '2020', '2021'],
+        datasets: [
+        {
+          label: 'População do Brasil (milhão)',
+          data: [211, 213, 214],
+          backgroundColor: [
+            'red',
+            '#3f51b5',
+            'yellow',
+          ],
+        },
+      ]
+      },
+    });
+
+
+
+    var chart = new Chart('meuCanvas6', {
+      type: 'bar',
+  data: {
+    labels: ["CS", "IT" , "ECE" , "EE", "ME", "BE"],
+    datasets: [
+      { label: '# of students',
+        data: [105,124,78,91,62,56],
+        backgroundColor :['rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+],
+  
+borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+borderWidth : 1
+      }
+    ]
+  },
+  options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+    });
+
+
+
     
   }
 
